5af667465685405d4ae8f69cc057f532099dc332
Akanshu Singh <akanshusingh0002@gmail.com>
2024-01-07 12:16:22 +0530

    feat: created vip site and created the plugin

---
+++ pmc-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: PMC Plugin
+ * Description: All backend functionality will take place in this plugin.
+ *              Like, creating custom cli commands.
+ * Plugin URI:  https://rtcamp.com
+ * Author:      rtCamp
+ * Author URI:  https://rtcamp.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Version:     1.0
+ * Text Domain: pmc-plugin
+ *
+ * @package PMC_Plugin
+ */
